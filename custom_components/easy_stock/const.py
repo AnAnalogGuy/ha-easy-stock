655a75d78d2455de217979a268f3a19034441111
AnAnalogGuy <70239317+AnAnalogGuy@users.noreply.github.com>
2026-08-06 23:03:54 +0200

    Request pre/post market data from Yahoo so market_state reports PRE/POST (fixes #13)

--- custom_components/easy_stock/const.py
+++ custom_components/easy_stock/const.py
@@ -1,17 +1,19 @@
-DOMAIN = "easy_stock"
-
-CONF_SYMBOL = "symbol"
-CONF_NAME = "name"
-CONF_SCAN_INTERVAL = "scan_interval"
-
-DEFAULT_SCAN_INTERVAL = 900  # 15 minutes
-
-YAHOO_CHART_URL = (
-    "https://query1.finance.yahoo.com/v8/finance/chart/{symbol}"
-    "?range=1y&interval=1d&includePrePost=false"
-)
-
-YAHOO_CHART_URL_MINI = (
-    "https://query1.finance.yahoo.com/v8/finance/chart/{symbol}"
-    "?range=5d&interval=1d&includePrePost=false"
-)
+1| DOMAIN = "easy_stock"
+2| 
+3| CONF_SYMBOL = "symbol"
+4| CONF_NAME = "name"
+5| CONF_SCAN_INTERVAL = "scan_interval"
+6| 
+7| DEFAULT_SCAN_INTERVAL = 900  # 15 minutes
+8| 
+9| YAHOO_CHART_URL = (
+10|     "https://query1.finance.yahoo.com/v8/finance/chart/{symbol}"
+11|-    "?range=1y&interval=1d&includePrePost=false"
+11|+    "?range=1y&interval=1d&includePrePost=true"
+12| )
+13| 
+14| YAHOO_CHART_URL_MINI = (
+15|     "https://query1.finance.yahoo.com/v8/finance/chart/{symbol}"
+16|-    "?range=5d&interval=1d&includePrePost=false"
+16|+    "?range=5d&interval=1d&includePrePost=true"
+17| )
